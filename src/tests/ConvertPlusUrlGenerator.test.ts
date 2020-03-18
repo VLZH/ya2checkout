1f1b43ef1c80037aae357ce10a1d2361ac530702
vlzh <vlzh@pm.me>
2020-03-18 18:20:43 +0600

    New tests for ConvertPlusUrlGenerator

--- src/tests/ConvertPlusUrlGenerator.test.ts
+++ src/tests/ConvertPlusUrlGenerator.test.ts
@@ -1,9 +1,20 @@
-import { ConvertPlusUrlGenerator } from "../utils";
 import * as qs from "qs";
 import * as url from "url";
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+import { ConvertPlusUrlGenerator } from "../utils";
 
 const getSearchFromUrl = (u: string) => {
     return (url.parse(u).search as string).slice(1);
+};
+
+const getGlobalObjectFromPage = async (url: string): Promise<any> => {
+    const response = await axios.get(url);
+    const dom = new JSDOM(response.data, {
+        runScripts: "dangerously"
+    });
+    return dom.window;
 };
 
 describe("ConvertPlusUrlGenerator", () => {
@@ -33,9 +44,51 @@ describe("ConvertPlusUrlGenerator", () => {
             }
         );
         const generated_url = gen.getUrl();
+        console.log(`generated_url: ${generated_url}`);
         // get only search part
         const generated_query = getSearchFromUrl(generated_url);
-        console.log(`generated_url: ${generated_url}`);
         expect(qs.parse(generated_query)).toEqual(qs.parse(expectedUrl));
+    });
+
+    it("Test generated link", async () => {
+        const gen = new ConvertPlusUrlGenerator(
+            process.env.MERCHANT_ID as string,
+            process.env.BUY_LINK_SECRET_WORD as string,
+            {
+                merchant: process.env.MERCHANT_ID as string,
+                dynamic: true,
+                currency: "USD",
+                tpl: "default",
+                prod: ["First product", "Second Product"],
+                tangible: true,
+                price: [10, 20],
+                type: "product",
+                qty: [1]
+            }
+        );
+        const page_globals = await getGlobalObjectFromPage(gen.getUrl());
+        expect(page_globals).toHaveProperty("$GLOBAL_VARS");
+        expect(page_globals.$GLOBAL_VARS).toHaveProperty("context");
+    });
+
+    it("Test generated link with incorrect currency", async () => {
+        const gen = new ConvertPlusUrlGenerator(
+            process.env.MERCHANT_ID as string,
+            process.env.BUY_LINK_SECRET_WORD as string,
+            {
+                merchant: process.env.MERCHANT_ID as string,
+                dynamic: true,
+                currency: "US",
+                tpl: "default",
+                prod: ["First product"],
+                tangible: true,
+                price: [10],
+                type: "product",
+                qty: [1]
+            }
+        );
+        const page_globals = await getGlobalObjectFromPage(gen.getUrl());
+        expect(page_globals).toHaveProperty("$GLOBAL_VARS");
+        expect(page_globals.$GLOBAL_VARS).not.toHaveProperty("context");
     });
 });
